34368bc5f4c5392719d4c2c05d3954013de14fc4
JinminKim <uiop3847@gmail.com>
2022-08-17 19:23:56 +0900

    update

---
+++ .vscode/p2475/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n1;
+    cout<<" enter the random number "<<endl;
+    cin >> n1;
+    cout << n1 << endl;
+
+    return 0;
+
+}
