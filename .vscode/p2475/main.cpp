867fd2d0f489ee6a702c2f8324ceefc2a25effaa
JinminKim <uiop3847@gmail.com>
2022-08-18 11:13:25 +0900

    update

--- .vscode/p2475/main.cpp
+++ .vscode/p2475/main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() 
 {
-    int n1;
-    cout<<" enter the random number "<<endl;
-    cin >> n1;
-    cout << n1 << endl;
+    int n1, n2, n3, n4, n5, num;
+    cin >> n1 >> n2 >> n3 >> n4 >> n5;
+    num = (n1*n1 + n2*n2 + n3*n3 + n4*n4 + n5*n5) % 10;
+    cout << num << endl;
 
     return 0;
 
