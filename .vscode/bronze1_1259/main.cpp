7054ccdb978920b1f1397633fa2f45e6288e1dd0
JinminKim <uiop3847@gmail.com>
2022-08-29 22:33:08 +0900

    .

--- .vscode/bronze1_1259/main.cpp
+++ .vscode/bronze1_1259/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() 
 {
-    
+    while (true) {
+        string num;
+        cin >> num;
+
+        if (num == "0") { break; }
+
+        int num_len = num.length();
+        bool no_button = false;
+
+        for (int i = 0 ; i < num_len / 2 ; i++) {
+            if (num[i] != num[num_len - i - 1]) {
+                no_button = true;
+                break;
+            }
+        }
+        
+        if ( no_button == true ) { cout << "no" << endl;}
+        else { cout << "yes" << endl; }
+
+    }
+
+    return 0;
 
 }
--- .vscode/bronze3_2884/main.cpp
+++ .vscode/bronze3_2884/main.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 int main() 
 {
-    
+    int H, M;
+    cin >> H >> M;
 
+    int H2, M2;
+
+    if ( M >= 45 ) {
+        H2 = H;
+        M2 = M - 45;
+    
+    }
+
+    else {
+        if ( H == 0 ){
+            H2 = H + 23;
+        }
+
+        else {
+            H2 = H - 1;
+        }
+        M2 = M + 15;
+
+    }
+
+    cout << H2 << " " << M2 << endl;
+
+    return 0;
 }
