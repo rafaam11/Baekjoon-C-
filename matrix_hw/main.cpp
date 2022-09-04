7d8d197c533b068d27e3706fea9c190000ee5a79
JinminKim <uiop3847@gmail.com>
2022-09-04 17:03:32 +0900

    .

---
+++ matrix_hw/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+
+struct Matrix 
+{
+    int w;
+    int h;
+    int* data;
+};
+
+Matrix* CreateMatrix(int w, int h)
+{
+    
+    return mat;
+
+}
+
+Matrix* Multiply(Matrix* a, Matrix* b)
+{
+
+}
+
+Matrix* Add(Matrix* a, Matrix* b)
+{
+
+}
+
+void DeleteMatrix(Matrix* a)
+{
+
+}
+
+int main() 
+{
+    int w1 = 2;
+    int h1 = 4;
+    int w2 = 4;
+    int h2 = 3;
+    Matrix* mat1 = CreateMatrix(w1, h1);
+    Matrix* mat2 = CreateMatrix(w2, h2);
+
+
+    return 0;
+
+}
