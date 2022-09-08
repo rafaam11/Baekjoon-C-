9495ca620108eea5d33d7d01860b75355ea853c6
rafaam11 <uiop3847@gmail.com>
2022-09-09 03:21:53 +0900

    .

--- stack_hw/main.cpp
+++ stack_hw/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-using namespace std;
 
 // "Stack"이라는 구조체의 선언문
 struct Stack               
@@ -12,169 +11,202 @@ struct Stack
 
 
 // 최대 크기가 max_size인 Stack을 생성
-Stack* CreateStack(unsigned int max_size);
+Stack* CreateStack(unsigned int max_size)
+{
+    Stack* stack = new Stack;
+    stack->max_size = max_size;
+    stack->stack_pointer = -1;
+    stack->data = new unsigned int[max_size];
 
-// Stack을 삭제
-void DeleteStack(Stack* st);
+    return stack;
+};
+
+// 동적할당된 Stack을 삭제
+void DeleteStack(Stack* st)
+{
+    delete[] st->data;
+    delete st;
+
+};
 
 // Stack src에 있는 모든 정보(최대 크기, 현재 크기, 정보)를 Stack dst로 복사
-void Copy(Stack* src, Stack* dst);
+void Copy(Stack* src, Stack* dst)
+{
+    dst->max_size = src->max_size;
+    dst->stack_pointer = src->stack_pointer;
+    for (int i = 0 ; i < dst->max_size ; ++i) {
+        dst->data[i] = src->data[i];
+    }
+
+};
 
 // Stack과 동일한 정보를 가지고 있는 Stack을 추가적으로 생성
-Stack* Clone(Stack* st);
+Stack* Clone(Stack* st)
+{
+    Stack* clone = new Stack;
+    clone->max_size = st->max_size;
+    clone->stack_pointer = st->stack_pointer;
+    clone->data = new unsigned int[clone->max_size];
+    for (int i = 0 ; i < clone->max_size ; ++i) {
+        clone->data[i] = st->data[i];
+    }
+
+    return clone;
+
+};
 
 // 데이터 x를 Stack의 가장 위에 추가
-void Push(Stack* st, unsigned int x);
+void Push(Stack* st, unsigned int x)
+{
+    if (st->stack_pointer == st->max_size-1) {
+        std::cout << "Stack이 가득 참. \n";
+    }
+
+    else {
+        st->data[++st->stack_pointer] = x;
+    }
+    
+};
 
 // Stack에서 가장 위에 있는 데이터를 삭제하고 반환, 만약 데이터가 없는 경우 -1 반환
-int Pop(Stack* st);
+int Pop(Stack* st)
+{
+    if (st->stack_pointer == -1) {
+        return -1;
+    }
+
+    else {
+        return st->data[st->stack_pointer--];
+    }
+};
 
 // Stack에서 가장 위에 있는 데이터를 반환(삭제하지는 않음), 만약 데이터가 없는 경우 -1 반환
-int Top(Stack* st);
+int Top(Stack* st)
+{
+    if (st->stack_pointer == -1) {
+        return -1;
+    }
+
+    else {
+        return st->data[st->stack_pointer];
+    }
+};
 
 // Stack의 최대 사용 가능한 크기 반환
-int MaxSize(Stack* st);
+int MaxSize(Stack* st) 
+{
+    return st->max_size - st->stack_pointer - 1;
+};
 
 // Stack의 현재 사용 중인 크기 반환
-unsigned int Size(Stack* st);
+unsigned int Size(Stack* st)
+{
+    return st->stack_pointer;
+};
 
 // Stack이 현재 비어있는지 여부를 반환
-bool isEmpty(Stack* st);
-
-
-
-Matrix* CreateMatrix(int width, int height)         // Matrix를 생성하는 함수
+bool isEmpty(Stack* st)
 {
-    Matrix* mat = new Matrix;
-    mat->w = width;
-    mat->h = height;
-    mat->data = new int[width*height];
-
-    return mat;
-
-}
-
-void fill(Matrix* mat, int value)           // Matrix의 데이터를 채우는 함수
-{
-    for (int width=0; width < mat->w; ++width) 
-        for (int height=0; height < mat->h; ++height) 
-            mat->data[width * mat->h + height] = value;
-}
-
-void fillArange(Matrix* mat, int value)           // Matrix의 데이터를 초기 value에서 1씩 더하여 채우는 함수
-{
-
-    for (int k=0; k < mat->h*mat->w; ++k) {
-        if (k == 0)
-            mat->data[k] = value;
-        else 
-            mat->data[k] = mat->data[k-1] + 1;
-
-    }
-
-}
-
-void PrintMatrix(Matrix* mat)               // Matrix의 모든 데이터 값을 출력하는 함수
-{
-    for (int width=0; width < mat->w; ++width) {
-        for (int height=0; height < mat->h; ++height) {
-            cout << mat->data[width * mat->h + height] << " ";
-        }
-        cout << " " << endl;
-    }   
-}
-
-Matrix* Multiply(Matrix* mat1, Matrix* mat2)        // Matrix 곱셈 연산을 하는 함수
-{
-    Matrix* mat3 = CreateMatrix(mat1->w, mat2->h);
-
-    if (mat1->h != mat2->w) {
-        cout << "*곱셈을 하려했지만 두 Matrix의 크기가 적절하지 않습니다. \n";
+    if (st->stack_pointer == -1) {
+        return true;
     }
 
     else {
-        for (int width=0; width < mat3->w; ++width) {
-            for (int height=0; height < mat3->h; ++height) {
-                int element_sum = 0;
-                for (int k=0; k < mat1->h; ++k) {           // k: 덧셈연산 반복수 (0~)
-                    element_sum += mat1->data[width * mat1->h + k] * mat2->data[k * mat2->h + height];
-                }
-                mat3->data[width * mat3->h + height] = element_sum;
-            }
-        }
+        return false;
     }
 
-    return mat3;
+};
 
-}
+// Stack을 프린트
+void PrintStack(Stack* st)
+{   
+    std::cout << "Max size : " << st->max_size << std::endl;
+    std::cout << "Top : " << st->stack_pointer+1 << std::endl;
+    std::cout << "Data : " ;
 
-Matrix* Add(Matrix* mat1, Matrix* mat2)             // Matrix 덧셈 연산을 하는 함수
-{
-    Matrix* mat3 = CreateMatrix(mat1->w, mat1->h);
-
-    if (mat1->w != mat2->w || mat1->h != mat2->h) {
-        cout << "*덧셈을 하려했지만 두 Matrix의 크기가 일치하지 않습니다. \n";
+    if (st->stack_pointer == -1) {
+        std::cout << "-1 \n";
     }
 
     else {
-        for (int width=0; width < mat3->w; ++width) {
-            for (int height=0; height < mat3->h; ++height) {
-                mat3->data[width * mat3->h + height] = mat1->data[width * mat1->h + height] + mat2->data[width * mat2->h + height];
+        for (int i ; i < st->max_size ; ++i) {
+            if (i <= st->stack_pointer) {
+                std::cout << st->data[i] << " ";
+            }
+
+            else {
+                std::cout << 0 << " ";
             }
         }
-
+        std::cout << std::endl;
     }
+    
+};
 
-    return mat3;
-
-}
-
-void DeleteMatrix(Matrix* mat)                  // 동적할당된 변수를 제거하는 함수
-{
-    delete[] mat->data;
-    delete mat;
-}
 
 int main() 
 {   
-    // Matrix 크기 선언
-    int w1, h1, w2, h2;
-    cout << "*mat1 크기 입력.\n";
-    cin >> w1 >> h1 ;
-    cout << "*mat2 크기 입력.\n";
-    cin >> w2 >> h2 ;
+    // St1의 최대크기 사용자 입력
+    int max_size;
+    std::cout << "St1의 최대크기 입력.\n";
+    std::cin >> max_size;
 
-    // mat1 생성 및 숫자 채우기
-    cout << "\n mat1 = \n" ;
-    Matrix* mat1 = CreateMatrix(w1, h1);
-    int val1 = 4; // mat2 데이터를 채울 값
-    fillArange(mat1, val1);
-    PrintMatrix(mat1);
+    // CreateStack 테스트
+    Stack* st1 = CreateStack(max_size);
+    std::cout << "\n @@@ st1 정보 출력 (CreateStack 테스트) \n";
+    PrintStack(st1);
 
-    // mat2 생성 및 숫자 채우기
-    cout << "\n mat2 = \n" ;
-    Matrix* mat2 = CreateMatrix(w2, h2);
-    int val2 = 10; // mat2 데이터를 채울 값
-    fillArange(mat2, val2);
-    PrintMatrix(mat2);
+    // Push 테스트
+    Push(st1, 1);
+    Push(st1, 3);
+    Push(st1, 5);
+    Push(st1, 7);
+    Push(st1, 9);
+    std::cout << "\n @@@ st1 정보 출력 (Push 테스트) \n";
+    PrintStack(st1);
 
-    // 곱셈 연산
-    cout << "\n mat1 x mat2 = \n" ;
-    Matrix* mat_mul = Multiply(mat1, mat2);
-    if (mat1->h == mat2->w) PrintMatrix(mat_mul);
+    // Pop 테스트
+    std::cout << "\n @@@ st1 정보 출력 (Pop 테스트) \n";
+    std::cout << "가장 위에 있던 데이터: " << Pop(st1) << std::endl;
+    PrintStack(st1);
 
-    // 덧셈 연산
-    cout << "\n mat1 + mat2 = \n" ;
-    Matrix* mat_add = Add(mat1, mat2);
-    if (mat1->w == mat2->w && mat1->h == mat2->h) PrintMatrix(mat_add);
+    // Top 테스트
+    std::cout << "\n @@@ st1 정보 출력 (Top 테스트) \n";
+    std::cout << "가장 위에 있는 데이터: " << Top(st1) << std::endl;
+    PrintStack(st1);
+
+    // Maxsize 테스트
+    std::cout << "\n @@@ st1 정보 출력 (Maxsize 테스트) \n";
+    std::cout << "최대 사용 가능한 크기 : " << MaxSize(st1) << std::endl;
+
+    // Size 테스트
+    std::cout << "\n @@@ st1 정보 출력 (Size 테스트) \n";
+    std::cout << "현재 사용 가능한 크기 : " << Size(st1) << std::endl;
+
+    // isEmpty 테스트
+    std::cout << "\n @@@ st1 정보 출력 (isEmpty 테스트) \n";
+    std::cout << "비어있는지 여부 (1이면 비어있음) : " << isEmpty(st1) << std::endl;
+
+    // Copy 테스트
+    Stack* st_copy = CreateStack(max_size + 10);
+    std::cout << "\n @@@ st_copy 정보 출력 (copy 전) \n";
+    PrintStack(st_copy);
+    Copy(st1, st_copy);
+    std::cout << "\n @@@ st_copy 정보 출력 (copy 후) \n";
+    PrintStack(st_copy);
+
+    // Clone 테스트
+    Stack* st_clone = Clone(st1);
+    std::cout << "\n @@@ st_clone 정보 출력 (Clone 테스트) \n";
+    PrintStack(st_copy);
+
 
     // 동적할당된 변수를 제거하고 마무리
-    DeleteMatrix(mat1);
-    DeleteMatrix(mat2);
-    DeleteMatrix(mat_add);
-    DeleteMatrix(mat_mul);
+    DeleteStack(st1);
+    DeleteStack(st_copy);
+    DeleteStack(st_clone);
+
 
 
     return 0;
-
 }
