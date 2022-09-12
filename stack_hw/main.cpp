3e0a57bb63b9643948b33eb4c998a1c7529a4347
rafaam11 <uiop3847@gmail.com>
2022-09-13 01:18:03 +0900

    Complete

--- stack_hw/main.cpp
+++ stack_hw/main.cpp
@@ -30,7 +30,7 @@ void DeleteStack(Stack* st)
 };
 
 // Stack src에 있는 모든 정보(최대 크기, 현재 크기, 정보)를 Stack dst로 복사
-void Copy(Stack* src, Stack* dst)
+void Copy(const Stack* src, Stack* dst)
 {
     dst->max_size = src->max_size;
     dst->stack_pointer = src->stack_pointer;
@@ -41,17 +41,15 @@ void Copy(Stack* src, Stack* dst)
 };
 
 // Stack과 동일한 정보를 가지고 있는 Stack을 추가적으로 생성
-Stack* Clone(Stack* st)
+Stack* Clone(const Stack* src)
 {
-    Stack* clone = new Stack;
-    clone->max_size = st->max_size;
-    clone->stack_pointer = st->stack_pointer;
-    clone->data = new unsigned int[clone->max_size];
-    for (int i = 0 ; i < clone->max_size ; ++i) {
-        clone->data[i] = st->data[i];
+    Stack* cln = CreateStack(src->max_size);
+    cln->stack_pointer = src->stack_pointer;
+    for (int i = 0 ; i < cln->max_size ; ++i) {
+        cln->data[i] = src->data[i];
     }
 
-    return clone;
+    return cln;
 
 };
 
@@ -59,7 +57,7 @@ Stack* Clone(Stack* st)
 void Push(Stack* st, unsigned int x)
 {
     if (st->stack_pointer == st->max_size-1) {
-        std::cout << "Stack이 가득 참. \n";
+        std::cout << "Stack이 가득 차서 Push할 수 없음. \n";
     }
 
     else {
@@ -101,11 +99,11 @@ int MaxSize(Stack* st)
 // Stack의 현재 사용 중인 크기 반환
 unsigned int Size(Stack* st)
 {
-    return st->stack_pointer;
+    return st->stack_pointer+1;
 };
 
 // Stack이 현재 비어있는지 여부를 반환
-bool isEmpty(Stack* st)
+bool isEmpty(const Stack* st)
 {
     if (st->stack_pointer == -1) {
         return true;
@@ -117,11 +115,24 @@ bool isEmpty(Stack* st)
 
 };
 
+// Stack이 가득 차있는지 여부를 반환
+bool isFull(const Stack* st)
+{
+    if (st->stack_pointer == st->max_size-1) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+
+};
+
 // Stack을 프린트
-void PrintStack(Stack* st)
+void PrintStack(const Stack* st)
 {   
     std::cout << "Max size : " << st->max_size << std::endl;
-    std::cout << "Top : " << st->stack_pointer+1 << std::endl;
+    std::cout << "Stack pointer : " << st->stack_pointer+1 << std::endl;
     std::cout << "Data : " ;
 
     if (st->stack_pointer == -1) {
@@ -135,7 +146,7 @@ void PrintStack(Stack* st)
             }
 
             else {
-                std::cout << 0 << " ";
+                std::cout << "* ";
             }
         }
         std::cout << std::endl;
@@ -147,22 +158,22 @@ void PrintStack(Stack* st)
 int main() 
 {   
     // St1의 최대크기 사용자 입력
-    int max_size;
     std::cout << "St1의 최대크기 입력.\n";
+    int max_size;
     std::cin >> max_size;
 
     // CreateStack 테스트
-    Stack* st1 = CreateStack(max_size);
     std::cout << "\n @@@ st1 정보 출력 (CreateStack 테스트) \n";
+    Stack* st1 = CreateStack(max_size);
     PrintStack(st1);
 
     // Push 테스트
+    std::cout << "\n @@@ st1 정보 출력 (Push 테스트) \n";
     Push(st1, 1);
     Push(st1, 3);
     Push(st1, 5);
     Push(st1, 7);
     Push(st1, 9);
-    std::cout << "\n @@@ st1 정보 출력 (Push 테스트) \n";
     PrintStack(st1);
 
     // Pop 테스트
@@ -177,36 +188,42 @@ int main()
 
     // Maxsize 테스트
     std::cout << "\n @@@ st1 정보 출력 (Maxsize 테스트) \n";
-    std::cout << "최대 사용 가능한 크기 : " << MaxSize(st1) << std::endl;
+    std::cout << "사용 가능한 최대 크기 : " << MaxSize(st1) << std::endl;
 
     // Size 테스트
     std::cout << "\n @@@ st1 정보 출력 (Size 테스트) \n";
-    std::cout << "현재 사용 가능한 크기 : " << Size(st1) << std::endl;
+    std::cout << "현재 사용 중인 크기 : " << Size(st1) << std::endl;
 
     // isEmpty 테스트
     std::cout << "\n @@@ st1 정보 출력 (isEmpty 테스트) \n";
-    std::cout << "비어있는지 여부 (1이면 비어있음) : " << isEmpty(st1) << std::endl;
+    if (isEmpty(st1) == 1) std::cout << "Stack이 비어있음 \n";
+    else std::cout << "Stack이 비어있지 않음 \n";
+
+    // isFull 테스트
+    std::cout << "\n @@@ st1 정보 출력 (isFull 테스트) \n";
+    if (isFull(st1) == 1) std::cout << "Stack이 가득 차 있음 \n";
+    else std::cout << "Stack이 가득 차 있지 않음 \n";
+    
 
     // Copy 테스트
     Stack* st_copy = CreateStack(max_size + 10);
-    std::cout << "\n @@@ st_copy 정보 출력 (copy 전) \n";
+    std::cout << "\n @@@ st_copy 정보 출력 (copy 전: 임의의 stack) \n";
     PrintStack(st_copy);
     Copy(st1, st_copy);
-    std::cout << "\n @@@ st_copy 정보 출력 (copy 후) \n";
+    std::cout << "\n @@@ st_copy 정보 출력 (copy 후: 카피된 stack) \n";
     PrintStack(st_copy);
 
     // Clone 테스트
-    Stack* st_clone = Clone(st1);
     std::cout << "\n @@@ st_clone 정보 출력 (Clone 테스트) \n";
-    PrintStack(st_copy);
+    Stack* st_clone = Clone(st1);
+    PrintStack(st_clone); // 여기서 Clone의 데이터가 왜 출력이 안되는 건지 잘 모르겠음.
 
 
     // 동적할당된 변수를 제거하고 마무리
+    std::cout << "\n @@@ 동적할당 변수 제거 \n";
     DeleteStack(st1);
     DeleteStack(st_copy);
     DeleteStack(st_clone);
-
-
 
     return 0;
 }
